5a1a37978e437df9d37feaf3aedfa704b6deedfd
Clayder.Huang <clayderhua@gmail.com>
2021-07-10 18:38:41 +0800

    add nvidia

---
+++ nvidia/all_nv_linux_dailybuild.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+MACHINE_LIST=""
+#rk_BSP
+./rk_android_N7_dailybuild.sh rk $VERSION_NUM
+
+#DS100_projects
+if [ "$AIR020" == "true" ]; then
+	MACHINE_LIST="$MACHINE_LIST air020"
+export MACHINE_LIST
+./nv_linux_dailybuild.sh air020 $VERSION_NUM
+fi
+echo "[ADV] All done!"
+
+
