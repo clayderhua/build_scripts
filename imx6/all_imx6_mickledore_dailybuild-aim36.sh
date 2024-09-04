be2f0120b5dce33a3c6938621523183469431878
clayderhua <clayderhua@gmail.com>
2024-09-04 16:07:24 +0800

    update

--- imx6/all_imx6_mickledore_dailybuild-aim36.sh
+++ imx6/all_imx6_mickledore_dailybuild-aim36.sh
@@ -9,17 +9,17 @@ $BUILD_SH imx6 imx6LBV"$VERSION_NUM" 1G
 [ "$?" -ne 0 ] && exit 1
 
 #imx6_projects
-if [ $RSB4411QDA1 == true ]; then
+if [ $RSB4411A1 == true ]; then
 	$BUILD_SH rsb4411a1-6q 4411A1"$AIM_VERSION"LIV"$VERSION_NUM" "1G" ""
 	[ "$?" -ne 0 ] && exit 1
 fi
 
-if [ $RSB4411DLA2 == true ]; then
+if [ $RSB4411A2_SOLO == true ]; then
         $BUILD_SH rsb4411a2-6dl 4411A1"$AIM_VERSION"LIV"$VERSION_NUM" "2G" ""
         [ "$?" -ne 0 ] && exit 1
 fi
 
-if [ $RSB3430QDA1 == true ]; then
+if [ $RSB3430A1 == true ]; then
         $BUILD_SH rsb3430a1-6q 3430A1"$AIM_VERSION"LIV"$VERSION_NUM" "1G" ""
         [ "$?" -ne 0 ] && exit 1
 fi
